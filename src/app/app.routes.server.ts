f6932e65a37f06842ae4d2c17dfe830581e533d6
nador <nader17124148@gmail.com>
2025-10-01 03:28:32 +0300

    refactor(routes): move getPrerenderParams function to app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -7,3 +7,18 @@ export const serverRoutes: ServerRoute[] = [
     // في الاغلب هنغير هنا البريريندر ل حاجه كلاينت كدا
   },
 ];
+
+// ✅ دالة getPrerenderParams اللي بتجيب البيانات ديناميك من الـ API
+export async function getPrerenderParams() {
+  const res = await fetch('https://ecommerce.routemisr.com/api/v1/products');
+  const data = await res.json();
+
+  // الـ API بيرجع البيانات في data.data (حسب وثائق RouteMisr)
+  const products = data.data;
+
+  // رجّع مصفوفة من كل القيم اللي Angular هيبني بيها الصفحات
+  return products.map((product: any) => ({
+    slug: product.slug,
+    id: product.id,
+  }));
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -59,18 +59,3 @@ export const routes: Routes = [
 
   { path: '**', component: NotfoundDetailsComponent, title: 'EROR page 404' },
 ];
-
-// ✅ دالة getPrerenderParams اللي بتجيب البيانات ديناميك من الـ API
-export async function getPrerenderParams() {
-  const res = await fetch('https://ecommerce.routemisr.com/api/v1/products');
-  const data = await res.json();
-
-  // الـ API بيرجع البيانات في data.data (حسب وثائق RouteMisr)
-  const products = data.data;
-
-  // رجّع مصفوفة من كل القيم اللي Angular هيبني بيها الصفحات
-  return products.map((product: any) => ({
-    slug: product.slug,
-    id: product.id,
-  }));
-}
